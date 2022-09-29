6d0f52e864a0abd2679388608cb287401b8a1473
Dimitrios <dpassias@student.bridgew.edu>
2022-09-29 13:32:56 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import arcade
+
+
+#arcade.get_window()
+my_window = arcade.open_window(1000, 800, "Shapes")
+arcade.set_background_color(arcade.color.BUFF)
+
+arcade.start_render()
+
+arcade.finish_render()
+
+arcade.run()
