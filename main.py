0b15b5e06b39698d10317c9229718efcdeeed91a
Dimitrios <dpassias@student.bridgew.edu>
2022-09-29 13:42:01 -0400

    Pumpkin Currently but needs to turn into another thing

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import arcade
 
 
-#arcade.get_window()
-my_window = arcade.open_window(1000, 800, "Shapes")
-arcade.set_background_color(arcade.color.BUFF)
 
+my_window = arcade.open_window(800, 800, "Pumpkin Face")
+arcade.set_background_color(arcade.color.DEEP_SPACE_SPARKLE)
 arcade.start_render()
+arcade.draw_circle_filled(400, 400, 300, arcade.color.PUMPKIN)
+arcade.draw_arc_outline(400, 300, 400, 100, arcade.color.EMINENCE, -180, 0, 8)
 
 arcade.finish_render()
 
